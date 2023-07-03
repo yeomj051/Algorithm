8f244f87a168598ada0549b4d8c4e9d9965f5229
YEO MINJI <43868492+yeomj051@users.noreply.github.com>
2023-07-03 23:51:35 +0900

    [Bronze I] Title: 애너그램, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/6996. 애너그램/애너그램.py
@@ -0,0 +1,14 @@
+test_case = int(input())
+
+for i in range(test_case):
+  right_word, anagrams = input().split()
+  right_list = list(right_word)
+  anagram_list = list(anagrams)
+
+  y_or_n = ''
+
+  for j in anagram_list:
+
+    if(right_list.count(j) != anagram_list.count(j) or len(right_list) != len(anagram_list)): y_or_n = 'NOT '
+
+  print(right_word + ' & ' + anagrams + ' are ' + y_or_n +  'anagrams.')
