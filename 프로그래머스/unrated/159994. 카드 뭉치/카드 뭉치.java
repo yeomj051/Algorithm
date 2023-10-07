080f39129c6a738ed8bfdc225980b9c6f20e8afd
YEO MINJI <43868492+yeomj051@users.noreply.github.com>
2023-10-07 19:04:53 +0900

    [unrated] Title: 카드 뭉치, Time: 0.16 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        
+        for(int i=0; i<cards1.length; i++){
+            queue1.add(cards1[i]);
+        }
+        for(int i=0; i<cards2.length; i++){
+            queue2.add(cards2[i]);
+        }
+        
+        for(int i=0; i<goal.length; i++){
+            if(goal[i].equals(queue1.peek())) queue1.poll();
+            else if(goal[i].equals(queue2.peek())) queue2.poll();
+            else {
+                answer = "No";
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
