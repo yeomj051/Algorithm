5e410fe654c1b9167942d09aa048bf10f1e7e9a8
YEO MINJI <43868492+yeomj051@users.noreply.github.com>
2023-10-25 11:54:33 +0900

    [Silver I] Title: 트럭, Time: 80 ms, Memory: 11852 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n,w,l;
+    public static int[] tList;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        tList = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            tList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int idx = 0;
+        int time = 0;
+        int weight = 0;
+        int doneIdx = 0;
+        int[] tChack = new int[n];
+        while(idx < n || doneIdx < n){
+            for(int i=doneIdx;i<idx;i++){
+                tChack[i]++;
+                if(tChack[i] > w){
+                    weight -= tList[i];
+                    doneIdx++;
+                }
+            }
+
+            if(idx < n && weight+tList[idx] <= l){
+                weight += tList[idx];
+                tChack[idx++]++;
+            }
+
+            time++;
+        }
+
+        System.out.println(time);
+    }
+}
