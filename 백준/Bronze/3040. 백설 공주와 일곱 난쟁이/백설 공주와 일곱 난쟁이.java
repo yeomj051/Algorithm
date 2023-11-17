56beef99ab3f82a00837c32c7e21d5efe6418c98
YEO MINJI <43868492+yeomj051@users.noreply.github.com>
2023-11-17 22:52:53 +0900

    Update 백설 공주와 일곱 난쟁이.java

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -10,6 +10,7 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
             sum += arr[i];
         }
+        
         for (int i = 0; i < 8; i++) {
             for (int j = i+1; j < 9; j++) {
                 int current = arr[i] + arr[j];
